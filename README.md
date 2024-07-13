40f47ee484104dc7993b6730d0a8d49a90745591
Andrew Lake <andrew@zealsprince.com>
2024-07-13 20:25:00 +0200

    Rewrite with bug fixes and Click for CLI

--- README.md
+++ README.md
@@ -1,42 +1,44 @@
-* Patreon: https://www.patreon.com/sd_hassan
 
 # civitai-image-scraper
-Downloads bulk images from Civitai that have a reaction count for a specific emoji reaction greater than your variable
+
+Downloads bulk images from Civitai
+
 Filters images in the response to only those that include a Prompt, saves the prompt as a caption text file matching the image name.
-Outputted results are almost suitable for training. LORA's are included in prompts so to do is a toggle to remove LORA's from the saved text file, for now you can remove them manually if using this for training
+Outputted results are almost suitable for training.
 
+LORA's are included in prompts so to do is a toggle to remove LORA's from the saved text file, for now you can remove them manually if using this for training
 
-# How to modify
-Line 10, `api_key = "xxxxxxx"` change to your API key from Civitai account settings.
+## Usage
 
-Line 20, `if image['stats']['heartCount'] > 10`
+Create a new virtual environment and install required modules by running the following commands.
 
-You can change the reaction type from heartCount to any other supported in the API 
-At the time of this script creation, the supported reaction types are:
+```bash
+python3 -m venv venv
+source venv/bin/activat
+pip install -r requirements.txt
+```
 
-        "cryCount"
+Run the script with the following command.
 
-        "laughCount"
-        
-        "likeCount"
-        
-        "dislikeCount"
-        
-        "heartCount"
-        
-        "commentCount"
-        
-https://github.com/civitai/civitai/wiki/REST-API-Reference#get-apiv1images
+```bash
+python3 civitai-image.py --help
 
-# How to run
-`pip install -r requirements.txt`
+Usage: civitai-image.py [OPTIONS]
 
-Replace line 10 API key with your API key
+  Download images from Civitai API.
 
-`python civitai-image.py`
-
-
-
-# example![brave_0a4zJ0eXOn](https://user-images.githubusercontent.com/119671806/231275932-66eff47f-1073-4ce5-a0e4-c90b5ff8a145.gif)
-
-
+Options:
+  --api-key TEXT              API key for Civitai
+  --download-path TEXT        Path to save the images
+  --min-width INTEGER         Minimum width of the image
+  --min-height INTEGER        Minimum height of the image
+  --max-images FLOAT          Maximum number of images to download
+  --require-metadata BOOLEAN  Only download images with metadata
+  --ignore-keywords TEXT      Comma separate list of keywords to ignore a
+                              result for
+                              
+  --nsfw BOOLEAN              Include NSFW images
+  --nsfw-only BOOLEAN         Only download NSFW images
+  
+  --help                      Show this message and exit.
+```
---
+++ civitai-scraper.py
@@ -0,0 +1,186 @@
+
+import os
+import re
+import math
+from io import BytesIO
+
+import click
+import requests
+
+from PIL import Image, UnidentifiedImageError
+
+INITIAL_URL = "https://civitai.com/api/v1/images?sort=Newest"
+
+DEFAULT_WORKERS = 4
+
+
+class FilterParams:
+    def __init__(self, min_width, min_height, min_hearts, metadata_required, nsfw_only):
+        self.min_width = min_width or 0
+        self.min_height = min_height or 0
+
+        self.min_hearts = min_hearts or 0
+
+        self.metadata_required = metadata_required or False
+
+        self.nsfw_only = nsfw_only or False
+
+
+def filter_items(items, downloaded, filter_params: FilterParams):
+    return [
+        item for item in items if
+        item['url'] + "\n" not in downloaded and
+        item['width'] >= filter_params.min_width and
+        item['height'] >= filter_params.min_height and
+        item['stats']['heartCount'] > filter_params.min_hearts and
+        (item['meta'] is not None if filter_params.metadata_required else True) and
+        (item['nsfw'] if filter_params.nsfw_only else True)
+    ]
+
+
+@click.command()
+@click.option("--api-key", help="API key for Civitai")
+@click.option("--download-path", default=".", help="Path to save the images")
+@click.option("--max-images",  default=math.inf, help="Maximum number of images to download")
+@click.option("--min-width", default=0, help="Minimum width of the image")
+@click.option("--min-height", default=0, help="Minimum height of the image")
+@click.option("--require-metadata", default=False, help="Only download images with metadata")
+@click.option("--ignore-keywords", default="", help="Comma separate list of keywords to ignore a result for")
+@click.option("--nsfw", default=False, help="Include NSFW images")
+@click.option("--nsfw-only", default=False, help="Only download NSFW images")
+def download(
+        api_key,
+        download_path,
+        max_images,
+        min_width,
+        min_height,
+        require_metadata,
+        ignore_keywords,
+        nsfw,
+        nsfw_only
+):
+    """Download images from Civitai API."""
+    # Check for API key
+    api_key = os.environ.get("CIVITAI_API_KEY") or api_key
+    if not api_key:
+        print("Please set the CIVITAI_API_KEY environment variable or pass it as an argument via --api-key.")
+
+        return
+
+    # Configuration
+    headers = {"Authorization": f"Bearer {api_key}"}
+
+    api_endpoint = INITIAL_URL
+    if nsfw or nsfw_only:
+        api_endpoint += "&nsfw=true"
+
+    # Ensure directory exists
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    # Regex to clean up prompt text
+    tag_re = re.compile(r'<.*?>')
+
+    # Store downloaded URLs to avoid duplicates
+    downloaded_urls_path = os.path.join(download_path, "downloaded.log")
+    downloaded_urls = set()
+
+    # Load downloaded URLs
+    if os.path.exists(downloaded_urls_path):
+        with open(downloaded_urls_path) as log_file:
+            downloaded_urls = set(log_file.readlines())
+
+    # Download images
+    with open(downloaded_urls_path, "a") as log_file:
+        next_url = api_endpoint
+        total_saved = 0
+
+        while next_url and total_saved < max_images:
+            response = requests.get(next_url, headers=headers)
+            response_json = response.json()
+
+            # Get next URL from metadata
+            if 'metadata' in response_json and 'nextPage' in response_json['metadata']:
+                next_url = response_json['metadata']['nextPage']
+            else:
+                next_url = None
+
+            filters = FilterParams(
+                min_width,
+                min_height,
+                0,
+                require_metadata,
+                nsfw_only
+            )
+
+            # Filter images
+            filtered_items = filter_items(
+                response_json['items'],
+                downloaded=downloaded_urls,
+                filter_params=filters
+            )
+
+            for item in filtered_items:
+                identifier = item['id']
+                url = item['url']
+
+                if item['meta'] is not None:
+                    if item['meta']['prompt'] is not None:
+                        # Skip items that contain a specific prompt
+                        if ignore_keywords != "":
+                            found = False
+                            for keyword in ignore_keywords.split(","):
+                                if keyword in item['meta']['prompt']:
+                                    print(
+                                        f"Skipping item {identifier} with matching ignore prompt. ({item['meta']['prompt']})")
+
+                                    found = True
+                                    break
+
+                            if found:
+                                continue
+
+                        # Save meta.prompt as a text file.
+                        meta_prompt = tag_re.sub('', item['meta']['prompt'])
+                        meta_filename = os.path.join(
+                            download_path, f"{identifier}.txt")
+
+                        with open(meta_filename, "w", encoding='utf-8') as meta_file:
+                            meta_file.write(meta_prompt)
+
+                # Download the next item (image/video)
+                item_response = requests.get(url)
+
+                # Attempt to download an image (not all URLs are images)
+                try:
+                    image = Image.open(BytesIO(item_response.content))
+
+                    # Convert image to RGB if necessary
+                    if image.mode in ['RGBA', 'P']:
+                        image = image.convert('RGB')
+
+                    # Save image
+                    img_filename = os.path.join(
+                        download_path, f"{identifier}.jpg")
+
+                    image.save(img_filename)
+
+                except UnidentifiedImageError:
+                    # Write teh content to a file
+                    with open(os.path.join(download_path, f"{identifier}.webm"), "wb") as file:
+                        file.write(item_response.content)
+
+                log_file.write(url + "\n")
+
+                downloaded_urls.add(url)
+
+                total_saved += 1
+
+                if total_saved >= max_images:
+                    break
+
+    print(f"Downloaded and saved {total_saved} images and metadata files.")
+
+
+if __name__ == "__main__":
+    download()
